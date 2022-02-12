97daf78fe7b34bd09cfdbcf76022a6dc2abeaf92
ghost-str <email@example.com>
2022-02-12 16:45:44 +0300

    login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,36 @@
+import {useState} from 'react';
 
 export default function Login() {
-    return <div> login </div>
+    const [loginForm, setLoginForm] = useState({login: '', password: ''});
+
+    const handleChange = (event) => {
+        const {id, value} = event.target;
+        setLoginForm((currentState) => ({
+            ...currentState,
+            [id]: value,
+        }));
+    }
+
+    const handleBlur = (event) => {
+        const {id, value} = event.target;
+        setLoginForm((currentState) => ({
+            ...currentState,
+            [id]: value.trim(),
+        }));
+    }
+
+    const handleLoginClick = (event) => {
+        event.preventDefault();
+        console.log('login', loginForm);
+    }
+
+
+    return <div>
+        <form>
+            <input type="text" id="login" onChange={handleChange} onBlur={handleBlur} value={loginForm.login}/>
+            <input type="password" id="password" onChange={handleChange} onBlur={handleBlur}
+                   value={loginForm.password}/>
+            <button onClick={handleLoginClick}> login</button>
+        </form>
+    </div>
 }
