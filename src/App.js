d2c4b69f076a310403957accd5030453f1a36a9f
ghost-str <email@example.com>
2022-02-12 16:00:44 +0300

    init common pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './pages/index';
+import Login from './pages/login';
+import Registration from './pages/registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/"  >
+              <Home />
+            </Route>
+            <Route exact path="/login"  >
+              <Login />
+            </Route>
+            <Route exact path="/registration"  >
+              <Registration />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
